cefc5c639fe8a3e8d2fb7bea56e009e8ea2becb7
Christopher Siu <christopher@siu.sh>
2022-04-26 13:23:09 -0700

    Added subroutine examples.

--- 04.26-subroutines/nested.asm
+++ 04.26-subroutines/nested.asm
@@ -3,9 +3,27 @@
 MAIN    JSR FOO
         HALT
 
-FOO     JSR BAR
+FOO     ST  R7, FOOR7   ; FOO is *both* caller and callee. It is therefore
+                        ;  responsible for saving all registers, including R7,
+                        ;  so that it can eventually return to MAIN.
+        JSR BAR
+        LD  R7, FOOR7   ; Restore R7.
         RET
 
-BAR     RET
+BAR     ST  R7, BARR7   ; BAR is also both caller and callee, and also needs to
+                        ;  save R7, but we must be careful not to overwrite the
+                        ;  existing saved value from FOO.
+        JSR FOO
+        LD  R7, BARR7   ; Restore R7.
+        RET
+
+BAY     RET
+
+; NOTE: This reserves memory for each subroutine *def'n*, not for each
+;       subroutine *call*. This scheme only works as long as we do not have
+;       multiple *calls* to the same subroutine ongoing simultaneously; i.e.,
+;       as long as no subroutine is recursive.
+FOOR7   .FILL x00       ; Space for FOO to save R7.
+BARR7   .FILL x00       ; Space for BAR to save R7.
 
         .END
--- 04.26-subroutines/sub.asm
+++ 04.26-subroutines/sub.asm
@@ -1,4 +1,26 @@
-; Computes R2 = R0 - R1.
-NOT R3, R1      ; Negate R1.
-ADD R3, R3, #1
-ADD R2, R0, R3  ; Add -R1 to R0.
+        .ORIG x3000
+
+        ; R2 and R7 are "caller-save", but we are not currently using them for
+        ;  anything meaningful, so we don't have to save them.
+        JSR SUB         ; Call SUB.
+        ; ...if we cared about R2 or R7, we would have to restore them here.
+
+        JSR SUB         ; Call SUB again.
+        HALT
+
+; Subtracts one register from another.
+;  Takes the minuend in R0, subtrahend in R1.
+;  Returns the difference in R2.
+SUB     ST  R3, SAVER3  ; Save R3 -- note that R3 is "callee-save", it may or
+                        ;  may not be meaningful, but we have no way of knowing,
+                        ;  so we have to save it regardless.
+
+        NOT R3, R1      ; Negate R1.
+        ADD R3, R3, #1
+        ADD R2, R0, R3  ; Add -R1 to R0.
+        LD  R3, SAVER3  ; Restore R3.
+        RET             ; Return.
+
+SAVER3  .FILL x00       ; Space in which to save R3.
+
+        .END
