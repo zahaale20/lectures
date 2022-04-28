1ae5267593a0ed1e4ef4a3575e65425565f541c8
Christopher Siu <christopher@siu.sh>
2022-04-28 13:22:32 -0700

    Added array and stack examples.

--- 04.28-arrays/array.asm
+++ 04.28-arrays/array.asm
@@ -1,5 +1,14 @@
         .ORIG x3000
 
+        LEA R1, ARR     ; R1 = ARR
+
+        AND R2, R2, #0  ; R2 = 5
+        ADD R2, R2, #5
+
+        STR R2, R1, #1  ; ARR[1] = 5
+
+        HALT
+
 ARR     .BLKW #4        ; An array of length 4
 
         .END
--- 04.28-arrays/stack.asm
+++ 04.28-arrays/stack.asm
@@ -1,5 +1,34 @@
         .ORIG x3000
 
+        ; NOTE: Stacks may grow from "low to high" or, equivalently, from
+        ;       "high to low". There is no asymptotic performance difference,
+        ;       but it is more convenient to go "high to low".
+        LEA R1, ARR     ; Initialize the stack pointer in R1.
+        ADD R1, R1, #4  ; Offset the stack pointer to be one location below
+                        ;  the "array" backing the stack.
+
+        ; Push 1:
+        AND R2, R2, #0
+        ADD R2, R2, #1
+        ADD R1, R1, #-1 ; Move the stack pointer *down* one location.
+        STR R2, R1, #0
+
+        ; Push 2:
+        ADD R2, R2, #1
+        ADD R1, R1, #-1 ; Move the stack pointer *down* one location.
+        STR R2, R1, #0
+
+        ; Push 3:
+        ADD R2, R2, #1
+        ADD R1, R1, #-1 ; Move the stack pointer *down* one location.
+        STR R2, R1, #0
+
+        ; Pop into R3:
+        LDR R3, R1, #0
+        ADD R1, R1, #1  ; Move the stack pointer *up* one location.
+
+        HALT
+
 ARR     .BLKW #4        ; An array of length 4
 
         .END
