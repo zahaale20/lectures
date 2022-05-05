838c0b15d6ced2d971e530eea4e5eba2cdf658a7
Christopher Siu <christopher@siu.sh>
2022-05-05 13:15:00 -0700

    Added ISR example.

--- 05.05-interrupts/echo.asm
+++ 05.05-interrupts/echo.asm
@@ -1,23 +1,29 @@
             .ORIG x1000     ; Place this service routine in the area of memory
                             ;  reserved for the OS. Note that we will also have
-                            ;  to set one of the TVT entries to 0x1000.
+                            ;  to set 0x0180 in the IVT to 0x1000.
 
-            ADD R6, R6, #-1 ; Push to R1 onto the supervisor stack.
-            STR R1, R6, #0
+            ADD R6, R6, #-1 ; Push R0 and R1 onto the supervisor stack; an ISR
+            STR R0, R6, #0  ;  has no caller. Everything is callee-save, as
+            ADD R6, R6, #-1 ;  the interrupted program cannot possibly know
+            STR R1, R6, #0  ;  when this service routine will run.
 
-POLLK       LDI R0, KBSR    ; Load the contents of the KBSR.
-            BRzp POLLK      ; Repeat until the user types a character.
+            LDI R0, KBDR    ; Load a character from the KBDR. We do not need to
+                            ;  poll the KBSR; the only way to get this far is
+                            ;  if the user already typed a character.
 
-            LDI R0, KBDR    ; Load a character from the KBDR. This is the one
-                            ;  situation where LDI is useful: for accessing a
-                            ;  constant address that doesn't fit in 9 bits.
+                            ; NOTE: Due to a bug in the LC-3 simulator, this is
+                            ;       the first point at which it is safe to set
+                            ;       breakpoints and step through instructions.
 
-POLLD       LDI R1, DSR     ; Load the contents of the DSR.
-            BRzp POLLD      ; Repeat until the display is ready for a character.
+POLLD       LDI R1, DSR     ; Poll the DSR. The display does not have support 
+            BRzp POLLD      ;  for interrupts, and we cannot add that in
+                            ;  software alone.
 
             STI R0, DDR     ; Store the character in the DDR.
 
-            LDR R1, R6, #0  ; Pop R1 off of the supervisor stack.
+            LDR R1, R6, #0  ; Pop R1 and R0 off of the supervisor stack.
+            ADD R6, R6, #1
+            LDR R0, R6, #0
             ADD R6, R6, #1
 
             RTI             ; Return.
